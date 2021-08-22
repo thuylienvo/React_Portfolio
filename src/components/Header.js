f7a3622486e5680ca8d24ac10d75aabc69f1b5a6
Mimi Vo <mimzy414@gmail.com>
2021-08-22 01:29:04 -0500

    header done

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useState } from 'react';
+import Nav from './Nav';
+
+function Header () {
+    return (
+      <header>
+        <Nav></Nav>
+      </header>
+    );
+  };
+
+
+export default Header;
