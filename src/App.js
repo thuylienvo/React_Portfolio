1b1097e3e7194b9836a9b3d926a3e6dfac2fdf6d
Mimi Vo <mimzy414@gmail.com>
2021-08-21 19:39:48 -0500

    added switch logic and return app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Nav from './components/Nav';
+import Header from './components/Header';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Portfolio from './page/Portfolio'
+
 
 function App() {
+  const [currentPage, handlePageChange] = useState('Home');
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'About':
+        return <About />;
+      case 'Portfolio':
+        return <Portfolio />;
+      case 'Contact':
+        return <Contact />;
+      case 'Resume':
+        return <Resume />;
+      default:
+        return <Home />
+    }
+  };
+
   return (
-    <div>
-    </div>
-  );
-}
+    <>
+    {/* Navigation */}
+    <Nav currentPage={currentPage} handlePageChange={handlePageChange}>
+      </Nav>
+    <main>
+      {renderPage(currentPage)}
+    </main>
+  </>
+  )
+};
 
 export default App;
